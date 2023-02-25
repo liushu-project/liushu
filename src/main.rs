fabb3f53bdbdc080a43b54c1235748d447c53575
Elliot <hack00mind@gmail.com>
2023-02-25 12:50:26 +0800

    break: remove compile command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use clap::{Parser, Subcommand};
 use liushu_core::deploy::deploy;
-use liushu_core::dict::compile_dict_to_db;
 use liushu_core::search::SearchEngine;
 
 #[derive(Parser, Debug)]
@@ -15,12 +14,6 @@ enum Commands {
     Deploy,
 
     #[command(arg_required_else_help = true)]
-    Compile {
-        dict: String,
-        target: String,
-    },
-
-    #[command(arg_required_else_help = true)]
     Query {
         code: String,
     },
@@ -33,7 +26,6 @@ fn main() {
         Commands::Deploy => {
             deploy();
         }
-        Commands::Compile { dict, target } => compile_dict_to_db(dict, target).unwrap(),
         Commands::Query { code } => {
             let engine = SearchEngine::new();
             let result = engine.search(code).unwrap();
