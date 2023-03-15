9c96c803e83a2d556a7ac1f19f1a038a15c0ffff
Elliot <hack00mind@gmail.com>
2023-03-15 10:47:20 +0800

    chore: compare two engine implements

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,23 +1,38 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use liushu_core::{
     dirs::PROJECT_DIRS,
     engine::{EngineWithRedb, InputMethodEngine, ShapeCodeEngine},
 };
 
-pub fn criterion_benchmark(c: &mut Criterion) {
+pub fn engine_benchmark(c: &mut Criterion) {
     let sunman = ShapeCodeEngine::default();
     let sunman2 = EngineWithRedb::with(&PROJECT_DIRS.target_dir).unwrap();
-    c.bench_function("engine with sqlite bench", |b| {
-        b.iter(|| {
-            sunman.search(black_box("hir")).unwrap();
-        })
-    });
-    c.bench_function("engine with redb bench", |b| {
-        b.iter(|| {
-            sunman2.search(black_box("hir")).unwrap();
-        })
-    });
+    let test_inputs = ["a", "aac", "bo", "cfl", "df", "fojq", "qiq", "hir", "zzz"];
+
+    let mut group = c.benchmark_group("Engine bench");
+    for input in test_inputs {
+        group.bench_with_input(
+            BenchmarkId::new("Engine with sqlite", input),
+            input,
+            |b, i| {
+                b.iter(|| {
+                    sunman.search(i).unwrap();
+                });
+            },
+        );
+        group.bench_with_input(
+            BenchmarkId::new("Engine with redb", input),
+            input,
+            |b, i| {
+                b.iter(|| {
+                    sunman2.search(i).unwrap();
+                });
+            },
+        );
+    }
+
+    group.finish();
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, engine_benchmark);
 criterion_main!(benches);
