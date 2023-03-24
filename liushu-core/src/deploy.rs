a41bc9df283b2ee7d54d0afa634577a621bea69c
Elliot <elliot.xu@coherencedigital.com>
2023-03-24 14:31:46 +0800

    feat: prepare target dir before deploy

--- liushu-core/src/deploy.rs
+++ liushu-core/src/deploy.rs
@@ -1,9 +1,11 @@
-use std::fs::File;
+use std::fs::{File, self};
 
 use crate::{config::Config, dirs::PROJECT_DIRS, engine::state::State, error::LiushuError};
 
 pub fn deploy() -> Result<(), LiushuError> {
     let config = Config::load();
+
+    fs::create_dir_all(&PROJECT_DIRS.target_dir)?;
 
     for formula in &config.formulas {
         formula.compile(&PROJECT_DIRS.config_dir, &PROJECT_DIRS.target_dir)?;
