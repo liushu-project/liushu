cc6a21bf0fb98859fc61ebfe7245031975151618
Elliot <hack00mind@gmail.com>
2023-06-28 23:32:03 +0800

    feat(android): add spacing between candidates

--- liushu-android/app/src/main/java/com/elliot00/liushu/KeyboardScreen.kt
+++ liushu-android/app/src/main/java/com/elliot00/liushu/KeyboardScreen.kt
@@ -22,13 +22,14 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
@@ -85,6 +86,7 @@ fun KeyboardScreen() {
                     input = ""
                     candidates = listOf()
                 })
+                Spacer(modifier = Modifier.width(8.dp))
             }
         }
         keysMatrix.forEach { row ->
