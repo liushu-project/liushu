56800e4a71046ec65bcb15353c4153adfe6d256b
Elliot <hack00mind@gmail.com>
2023-08-20 20:11:32 +0800

    chore(ci): android nix shell

--- flake.nix
+++ flake.nix
@@ -13,7 +13,35 @@
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
+          config = {
+            android_sdk.accept_license = true;
+            allowUnfree = true;
+          };
         };
+
+        buildToolsVersion = "31.0.0";
+        cmakeVersion = "3.18.1";
+        ndkVersion = "25.2.9519653";
+        androidComposition = pkgs.androidenv.composeAndroidPackages {
+          toolsVersion = "26.1.1";
+          platformToolsVersion = "33.0.3";
+          buildToolsVersions = [ buildToolsVersion ];
+          includeEmulator = false;
+          platformVersions = [ "33" ];
+          includeSources = false;
+          includeSystemImages = false;
+          systemImageTypes = [ "google_apis_playstore" ];
+          abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
+          cmakeVersions = [ cmakeVersion ];
+          includeNDK = true;
+          ndkVersions = [ ndkVersion ];
+          useGoogleAPIs = false;
+          useGoogleTVAddOns = false;
+          includeExtras = [
+            "extras;google;gcm"
+          ];
+        };
+        android-sdk = androidComposition.androidsdk;
       in
       with pkgs;
       {
@@ -37,8 +65,20 @@
           };
         };
 
-        devShells.default = mkShell {
+        devShells.default = mkShell rec {
+          ANDROID_SDK_ROOT = "${android-sdk}/libexec/android-sdk";
+          NDK_VERSION = ndkVersion;
+          ANDROID_NDK_ROOT = "${ANDROID_SDK_ROOT}/ndk-bundle";
+          GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/${buildToolsVersion}/aapt2";
+          JAVA_HOME = "${pkgs.jdk17.home}";
+
+          shellHook = ''
+            export PATH="$ANDROID_SDK_ROOT/cmake/${cmakeVersion}/bin:$PATH"
+            echo sdk.dir=$ANDROID_SDK_ROOT > liushu-android/local.properties
+          '';
+
           buildInputs = [
+            android-sdk
             openssl
             pkg-config
             dhall
