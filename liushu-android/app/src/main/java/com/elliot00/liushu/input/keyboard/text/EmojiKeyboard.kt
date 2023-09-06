f7e96b184aa420f16d5258580656894a1f1dc1a6
Elliot <hack00mind@gmail.com>
2023-09-06 21:07:15 +0800

    feat(android): emoji pager indicator

--- liushu-android/app/src/main/java/com/elliot00/liushu/input/keyboard/text/EmojiKeyboard.kt
+++ liushu-android/app/src/main/java/com/elliot00/liushu/input/keyboard/text/EmojiKeyboard.kt
@@ -18,24 +18,34 @@
 package com.elliot00.liushu.input.keyboard.text
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.elliot00.liushu.input.keyboard.KeyCode
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -49,19 +59,35 @@ fun EmojiKeyboard(onKeyPressed: (KeyCode) -> Unit) {
                 modifier = Modifier.height(260.dp)
             ) {
                 items(EMOJI_MATRIX[page].second) { emoji ->
-                    Text(text = emoji, fontSize = 24.sp,
-                        modifier = Modifier.clickable { onKeyPressed(KeyCode.RawText(emoji)) })
+                    TextButton(onClick = { onKeyPressed(KeyCode.RawText(emoji)) }) {
+                        Text(text = emoji, fontSize = 24.sp)
+                    }
                 }
             }
-
         }
-        IconButton(onClick = { onKeyPressed(KeyCode.Abc) }) {
-            Icon(Icons.Filled.KeyboardArrowLeft, "goBack")
+
+        val coroutineScope = rememberCoroutineScope()
+        LazyRow(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            item {
+                IconButton(onClick = { onKeyPressed(KeyCode.Abc) }) {
+                    Icon(Icons.Filled.KeyboardArrowLeft, "goBack")
+                }
+            }
+            itemsIndexed(EMOJI_MATRIX) { index, (category, _) ->
+                val color =
+                    if (pagerState.currentPage == index) Color.LightGray else MaterialTheme.colorScheme.background
+                FilledTonalButton(
+                    onClick = { coroutineScope.launch { pagerState.scrollToPage(index) } },
+                    colors = ButtonDefaults.filledTonalButtonColors(containerColor = color)
+                ) {
+                    Text(category, fontSize = 24.sp)
+                }
+            }
         }
     }
 }
 
-val EMOJI_MATRIX = listOf(
+val EMOJI_MATRIX = arrayOf(
     "😃" to arrayOf(
         "😀",
         "😃",
