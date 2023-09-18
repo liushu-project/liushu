ce0de1922eeee4eb536b0db5bb01151ee5fa99f5
Elliot <hack00mind@gmail.com>
2023-09-18 21:02:25 +0800

    chore(android): reset candidates font weight

--- liushu-android/app/src/main/java/com/elliot00/liushu/input/keyboard/candidate/CandidateItem.kt
+++ liushu-android/app/src/main/java/com/elliot00/liushu/input/keyboard/candidate/CandidateItem.kt
@@ -23,7 +23,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,7 +37,7 @@ fun CandidateItem(candidate: Candidate, onClick: () -> Unit = {}) {
             .clickable(onClick = onClick)
     ) {
         Text(
-            text = candidate.text, textAlign = TextAlign.Center, fontWeight = FontWeight.Bold
+            text = candidate.text, textAlign = TextAlign.Center
         )
     }
 
