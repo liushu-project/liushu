2947112def1fe0e9b407f40e0dd32a084356ff01
Elliot <hack00mind@gmail.com>
2023-10-30 19:18:09 +0800

    refactor(android): reuse `Key` component

--- liushu-android/app/src/main/java/com/elliot00/liushu/input/keyboard/Key.kt
+++ liushu-android/app/src/main/java/com/elliot00/liushu/input/keyboard/Key.kt
@@ -17,31 +17,205 @@
 
 package com.elliot00.liushu.input.keyboard
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import com.elliot00.liushu.R
 
 @Composable
-fun Key(
+fun RowScope.Key(
     data: KeyData,
     onKeyPressed: (KeyCode) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Row(
-        modifier = modifier
-            .fillMaxHeight()
-            .clickable {
-                onKeyPressed(data.keyCode)
-            },
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
-    ) {
-        Text(data.label)
+    when (data.keyCode) {
+        is KeyCode.Enter -> {
+            Row(
+                modifier = modifier
+                    .background(
+                        color = MaterialTheme.colorScheme.tertiaryContainer,
+                        shape = MaterialTheme.shapes.extraLarge
+                    )
+                    .clip(shape = MaterialTheme.shapes.extraLarge)
+                    .weight(1.5f)
+                    .clickable {
+                        onKeyPressed(data.keyCode)
+                    },
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    painterResource(id = R.drawable.ic_baseline_keyboard_return_24),
+                    contentDescription = "return",
+                    tint = MaterialTheme.colorScheme.onTertiaryContainer
+                )
+            }
+        }
+
+        is KeyCode.Shift -> {
+            Row(
+                modifier = modifier
+                    .background(
+                        color = MaterialTheme.colorScheme.primaryContainer,
+                        shape = MaterialTheme.shapes.medium
+                    )
+                    .clip(shape = MaterialTheme.shapes.medium)
+                    .weight(1.5f)
+                    .clickable {
+                        onKeyPressed(data.keyCode)
+                    },
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_capslock_none),
+                    contentDescription = "shift",
+                    tint = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            }
+        }
+
+        is KeyCode.Delete -> {
+            Row(
+                modifier = modifier
+                    .background(
+                        color = MaterialTheme.colorScheme.primaryContainer,
+                        shape = MaterialTheme.shapes.medium
+                    )
+                    .clip(shape = MaterialTheme.shapes.medium)
+                    .weight(1.5f)
+                    .clickable {
+                        onKeyPressed(data.keyCode)
+                    },
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_outline_backspace_24),
+                    contentDescription = "delete",
+                    tint = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            }
+        }
+
+        is KeyCode.Comma, is KeyCode.Period -> {
+            Row(
+                modifier = modifier
+                    .background(
+                        color = MaterialTheme.colorScheme.primaryContainer,
+                        shape = MaterialTheme.shapes.medium
+                    )
+                    .clip(shape = MaterialTheme.shapes.medium)
+                    .weight(1f)
+                    .clickable {
+                        onKeyPressed(data.keyCode)
+                    },
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = data.label,
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            }
+        }
+
+        is KeyCode.Symbols -> {
+            Row(
+                modifier = modifier
+                    .background(
+                        color = MaterialTheme.colorScheme.secondaryContainer,
+                        shape = MaterialTheme.shapes.extraLarge
+                    )
+                    .clip(shape = MaterialTheme.shapes.extraLarge)
+                    .weight(1.5f)
+                    .clickable {
+                        onKeyPressed(data.keyCode)
+                    },
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = data.label,
+                    color = MaterialTheme.colorScheme.onSecondaryContainer
+                )
+            }
+        }
+
+        is KeyCode.Space -> {
+            Row(
+                modifier = modifier
+                    .background(
+                        color = MaterialTheme.colorScheme.surface,
+                        shape = MaterialTheme.shapes.medium
+                    )
+                    .clip(shape = MaterialTheme.shapes.medium)
+                    .weight(4f)
+                    .clickable {
+                        onKeyPressed(data.keyCode)
+                    },
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {}
+        }
+
+        is KeyCode.Emoji -> {
+            Row(
+                modifier = modifier
+                    .background(
+                        color = MaterialTheme.colorScheme.surface,
+                        shape = MaterialTheme.shapes.medium
+                    )
+                    .clip(shape = MaterialTheme.shapes.medium)
+                    .weight(1f)
+                    .clickable {
+                        onKeyPressed(data.keyCode)
+                    },
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_outline_tag_faces_24),
+                    contentDescription = "emoji",
+                    tint = MaterialTheme.colorScheme.onSurface
+                )
+            }
+        }
+
+        else -> {
+            Row(
+                modifier = modifier
+                    .background(
+                        color = MaterialTheme.colorScheme.surface,
+                        shape = MaterialTheme.shapes.medium
+                    )
+                    .clip(shape = MaterialTheme.shapes.medium)
+                    .weight(1f)
+                    .clickable {
+                        onKeyPressed(data.keyCode)
+                    },
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = data.label,
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+        }
+
     }
 }
 
