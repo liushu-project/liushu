95fd7cdab15a18b2409438c7c4d0be586561a4e3
Elliot <hack00mind@gmail.com>
2023-11-11 19:01:49 +0800

    chore: split android modult into a separate repo

--- flake.nix
+++ flake.nix
@@ -13,83 +13,22 @@
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
-          config = {
-            android_sdk.accept_license = true;
-            allowUnfree = true;
-          };
-        };
-
-        buildToolsVersion = "31.0.0";
-        cmakeVersion = "3.18.1";
-        ndkVersion = "25.2.9519653";
-        androidComposition = pkgs.androidenv.composeAndroidPackages {
-          toolsVersion = "26.1.1";
-          platformToolsVersion = "33.0.3";
-          buildToolsVersions = [ buildToolsVersion ];
-          includeEmulator = false;
-          platformVersions = [ "33" ];
-          includeSources = false;
-          includeSystemImages = false;
-          systemImageTypes = [ "google_apis_playstore" ];
-          abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
-          cmakeVersions = [ cmakeVersion ];
-          includeNDK = true;
-          ndkVersions = [ ndkVersion ];
-          useGoogleAPIs = false;
-          useGoogleTVAddOns = false;
-          includeExtras = [
-            "extras;google;gcm"
-          ];
-        };
-        android-sdk = androidComposition.androidsdk;
-        my-rust = pkgs.rust-bin.stable."1.71.1".default.override {
-          extensions = [ "rust-src" ];
-          targets = [
-            "armv7-linux-androideabi"
-            "i686-linux-android"
-            "x86_64-linux-android"
-            "aarch64-linux-android"
-          ];
         };
       in
       with pkgs;
       {
-        packages = {
-          default = pkgs.rustPlatform.buildRustPackage {
-            pname = "liushu";
-            version = "0.1.0";
-            src = ./.;
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-            };
-          };
-        };
-
         devShells = {
           default = mkShell {
             buildInputs = [
-              my-rust
-            ];
-          };
-
-          android = mkShell rec {
-            ANDROID_SDK_ROOT = "${android-sdk}/libexec/android-sdk";
-            NDK_VERSION = ndkVersion;
-            ANDROID_NDK_ROOT = "${ANDROID_SDK_ROOT}/ndk-bundle";
-            GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/${buildToolsVersion}/aapt2";
-            JAVA_HOME = "${pkgs.jdk17.home}";
-
-            shellHook = ''
-              export PATH="$ANDROID_SDK_ROOT/cmake/${cmakeVersion}/bin:$PATH"
-              echo sdk.dir=$ANDROID_SDK_ROOT > liushu-android/local.properties
-            '';
-
-            buildInputs = [
-              android-sdk
-              openssl
-              pkg-config
-              dhall
-              my-rust
+              (pkgs.rust-bin.stable."1.71.1".default.override {
+                extensions = [ "rust-src" ];
+                targets = [
+                  "armv7-linux-androideabi"
+                  "i686-linux-android"
+                  "x86_64-linux-android"
+                  "aarch64-linux-android"
+                ];
+              })
             ];
           };
         };
