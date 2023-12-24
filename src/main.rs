865be7ace90292cbc161841275f4b8840e12aeb0
Elliot <hack00mind@gmail.com>
2023-12-24 11:34:43 +0800

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,133 @@
-use std::io::{stdin, stdout, Write};
-
-use clap::{Parser, Subcommand};
-use liushu_core::dirs::PROJECT_DIRS;
-use liushu_core::engine::{Engine, InputMethodEngine};
-
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Cli {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Debug, Subcommand)]
-enum Commands {
-    Repl,
-}
+use wayland_client::{
+    event_created_child,
+    protocol::{wl_keyboard, wl_registry, wl_seat},
+    Connection, Dispatch, Proxy, QueueHandle, WEnum,
+};
+use wayland_protocols::wp::input_method::zv1::client::{
+    zwp_input_method_context_v1,
+    zwp_input_method_v1::{self, EVT_ACTIVATE_OPCODE},
+};
 
 fn main() {
-    let args = Cli::parse();
+    let conn = Connection::connect_to_env().unwrap();
 
-    match args.command {
-        Commands::Repl => {
-            let engine = Engine::init(&PROJECT_DIRS).unwrap();
+    let mut event_queue = conn.new_event_queue();
+    let qhandle = event_queue.handle();
 
-            loop {
-                print!("liushu> ");
-                stdout().flush().unwrap();
-                let mut input = String::new();
-                match stdin().read_line(&mut input) {
-                    Ok(_) => {
-                        let input = input.trim();
+    let display = conn.display();
+    display.get_registry(&qhandle, ());
 
-                        if input == "*quit" {
-                            break;
-                        }
-
-                        engine
-                            .search(input)
-                            .unwrap_or_else(|e| {
-                                println!("error: {}", e);
-                                vec![]
-                            })
-                            .iter()
-                            .take(8)
-                            .enumerate()
-                            .for_each(|(i, result)| {
-                                println!("result{}: {:?}", i, result);
-                            });
-                    }
-                    Err(error) => println!("error: {}", error),
-                }
-            }
-        }
+    let mut state = AppState {
+        running: true,
+        ..Default::default()
     };
+
+    while state.running {
+        event_queue.blocking_dispatch(&mut state).unwrap();
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use crate::Cli;
-    use clap::CommandFactory;
+#[derive(Default)]
+struct AppState {
+    running: bool,
+    input_method: Option<zwp_input_method_v1::ZwpInputMethodV1>,
+    context: Option<zwp_input_method_context_v1::ZwpInputMethodContextV1>,
+}
 
-    #[test]
-    fn verify_cli() {
-        Cli::command().debug_assert()
+impl Dispatch<wl_registry::WlRegistry, ()> for AppState {
+    fn event(
+        state: &mut Self,
+        registry: &wl_registry::WlRegistry,
+        event: <wl_registry::WlRegistry as wayland_client::Proxy>::Event,
+        data: &(),
+        conn: &Connection,
+        qh: &QueueHandle<Self>,
+    ) {
+        if let wl_registry::Event::Global {
+            name, interface, ..
+        } = event
+        {
+            println!("{} {}", name, interface);
+            match &interface[..] {
+                "zwp_input_method_v1" => {
+                    let input_method = registry
+                        .bind::<zwp_input_method_v1::ZwpInputMethodV1, _, _>(name, 1, qh, ());
+                    state.input_method = Some(input_method);
+                }
+                _ => {}
+            }
+        }
+    }
+}
+
+impl Dispatch<zwp_input_method_v1::ZwpInputMethodV1, ()> for AppState {
+    fn event(
+        state: &mut Self,
+        proxy: &zwp_input_method_v1::ZwpInputMethodV1,
+        event: zwp_input_method_v1::Event,
+        data: &(),
+        conn: &Connection,
+        qhandle: &QueueHandle<Self>,
+    ) {
+        println!("current event is {:#?}", event);
+        match event {
+            zwp_input_method_v1::Event::Activate { id } => {
+                println!("method activate");
+                id.grab_keyboard(qhandle, ());
+                state.context = Some(id);
+                println!("grab keyboard");
+            }
+            zwp_input_method_v1::Event::Deactivate { context } => {
+                state.context = None;
+                context.destroy();
+                println!("method inactive");
+            }
+            _ => {}
+        }
+    }
+
+    event_created_child!(AppState, zwp_input_method_v1::ZwpInputMethodV1, [
+        EVT_ACTIVATE_OPCODE => (zwp_input_method_context_v1::ZwpInputMethodContextV1, ()),
+    ]);
+}
+
+impl Dispatch<zwp_input_method_context_v1::ZwpInputMethodContextV1, ()> for AppState {
+    fn event(
+        state: &mut Self,
+        context: &zwp_input_method_context_v1::ZwpInputMethodContextV1,
+        event: zwp_input_method_context_v1::Event,
+        data: &(),
+        conn: &Connection,
+        qhandle: &QueueHandle<Self>,
+    ) {
+        println!("current content event is {:#?}", event);
+        match event {
+            zwp_input_method_context_v1::Event::SurroundingText {
+                text,
+                cursor,
+                anchor,
+            } => {
+                println!("{} {} {}", text, cursor, anchor);
+            }
+            _ => {}
+        }
+    }
+}
+
+impl Dispatch<wl_keyboard::WlKeyboard, ()> for AppState {
+    fn event(
+        state: &mut Self,
+        proxy: &wl_keyboard::WlKeyboard,
+        event: wl_keyboard::Event,
+        data: &(),
+        conn: &Connection,
+        qhandle: &QueueHandle<Self>,
+    ) {
+        match event {
+            wl_keyboard::Event::Key { key, .. } => {
+                println!("{}", key);
+            }
+            _ => {}
+        }
     }
 }
