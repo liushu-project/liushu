b2803d130926ca0c60e7d107407456f38d88dfd9
Elliot <hack00mind@gmail.com>
2024-03-07 20:08:53 +0800

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use wayland_client::{
     event_created_child,
-    protocol::{wl_keyboard, wl_registry, wl_seat},
-    Connection, Dispatch, Proxy, QueueHandle, WEnum,
+    protocol::{
+        wl_keyboard::{self, KeyState},
+        wl_registry,
+    },
+    Connection, Dispatch, QueueHandle, WEnum,
 };
 use wayland_protocols::wp::input_method::zv1::client::{
     zwp_input_method_context_v1,
@@ -39,8 +42,8 @@ impl Dispatch<wl_registry::WlRegistry, ()> for AppState {
         state: &mut Self,
         registry: &wl_registry::WlRegistry,
         event: <wl_registry::WlRegistry as wayland_client::Proxy>::Event,
-        data: &(),
-        conn: &Connection,
+        _data: &(),
+        _conn: &Connection,
         qh: &QueueHandle<Self>,
     ) {
         if let wl_registry::Event::Global {
@@ -63,10 +66,10 @@ impl Dispatch<wl_registry::WlRegistry, ()> for AppState {
 impl Dispatch<zwp_input_method_v1::ZwpInputMethodV1, ()> for AppState {
     fn event(
         state: &mut Self,
-        proxy: &zwp_input_method_v1::ZwpInputMethodV1,
+        _proxy: &zwp_input_method_v1::ZwpInputMethodV1,
         event: zwp_input_method_v1::Event,
-        data: &(),
-        conn: &Connection,
+        _data: &(),
+        _conn: &Connection,
         qhandle: &QueueHandle<Self>,
     ) {
         println!("current event is {:#?}", event);
@@ -93,12 +96,12 @@ impl Dispatch<zwp_input_method_v1::ZwpInputMethodV1, ()> for AppState {
 
 impl Dispatch<zwp_input_method_context_v1::ZwpInputMethodContextV1, ()> for AppState {
     fn event(
-        state: &mut Self,
-        context: &zwp_input_method_context_v1::ZwpInputMethodContextV1,
+        _state: &mut Self,
+        _context: &zwp_input_method_context_v1::ZwpInputMethodContextV1,
         event: zwp_input_method_context_v1::Event,
-        data: &(),
-        conn: &Connection,
-        qhandle: &QueueHandle<Self>,
+        _data: &(),
+        _conn: &Connection,
+        _qhandle: &QueueHandle<Self>,
     ) {
         println!("current content event is {:#?}", event);
         match event {
@@ -117,15 +120,63 @@ impl Dispatch<zwp_input_method_context_v1::ZwpInputMethodContextV1, ()> for AppS
 impl Dispatch<wl_keyboard::WlKeyboard, ()> for AppState {
     fn event(
         state: &mut Self,
-        proxy: &wl_keyboard::WlKeyboard,
+        _proxy: &wl_keyboard::WlKeyboard,
         event: wl_keyboard::Event,
-        data: &(),
-        conn: &Connection,
-        qhandle: &QueueHandle<Self>,
+        _data: &(),
+        _conn: &Connection,
+        _qhandle: &QueueHandle<Self>,
     ) {
         match event {
-            wl_keyboard::Event::Key { key, .. } => {
-                println!("{}", key);
+            wl_keyboard::Event::Key {
+                key,
+                state: WEnum::Value(KeyState::Pressed),
+                ..
+            } => {
+                let new_key = key + 1;
+
+                let key_string = match new_key {
+                    16 => "q",
+                    17 => "w",
+                    18 => "e",
+                    19 => "r",
+                    20 => "t",
+                    21 => "y",
+                    22 => "u",
+                    23 => "i",
+                    24 => "o",
+                    25 => "p",
+                    26 => "[",
+                    27 => "]",
+                    28 => "\n",
+                    30 => "a",
+                    31 => "s",
+                    32 => "d",
+                    33 => "f",
+                    34 => "g",
+                    35 => "h",
+                    36 => "j",
+                    37 => "k",
+                    38 => "l",
+                    39 => ";",
+                    40 => "'",
+                    41 => "`",
+                    42 => "\\",
+                    44 => "z",
+                    45 => "x",
+                    46 => "c",
+                    47 => "v",
+                    48 => "b",
+                    49 => "n",
+                    50 => "m",
+                    51 => ",",
+                    52 => ".",
+                    53 => "/",
+                    _ => "",
+                };
+
+                if let Some(context) = &state.context {
+                    context.commit_string(1, key_string.to_string());
+                }
             }
             _ => {}
         }
